87158f53fcdda0a9951c99f91674bf95f7363963
asushma6112000-gif <sushmasush20016@gmail.com>
2026-08-28 15:57:58 +0530

    Restore RDS backend application code

--- aws-three-tier-architecture/backend/app.py
+++ aws-three-tier-architecture/backend/app.py
@@ -1,12 +1,43 @@
 from flask import Flask
+import os
+import pymysql
 
 app = Flask(__name__)
 
 
+def get_db_connection():
+    return pymysql.connect(
+        host=os.environ.get("DB_HOST"),
+        user=os.environ.get("DB_USERNAME"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
+        port=3306,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+
 @app.route("/")
-def health_check():
-  return "Healthy", 200
+def home():
+    return "Backend Flask Application is Running Successfully"
+
+
+@app.route("/api/")
+def api():
+    try:
+        connection = get_db_connection()
+        connection.close()
+
+        return {
+            "status": "success",
+            "message": "Backend connected to RDS successfully"
+        }
+
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }, 500
 
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
